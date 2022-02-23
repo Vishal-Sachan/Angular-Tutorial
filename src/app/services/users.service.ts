d7f1973ace3b054e0977242a2032aae8314cd731
Vishal-Sachan <vishal.sachan@credextechnology.com>
2022-02-23 15:12:06 +0530

    updated

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../components/users/users';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  private _url: string = "https://reqres.in/api/users";
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(this._url);
+  }
+}
