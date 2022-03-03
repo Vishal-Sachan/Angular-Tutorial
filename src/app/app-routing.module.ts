da0f4b88c518cc29e4b0b78192bb8749d949efd0
Vishal-Sachan <vishal.sachan@credextechnology.com>
2022-03-03 16:53:23 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { DirectivesComponent } from './components/directives/directives.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { AboutComponent } from './components/about/about.component';
-import { HomepageComponent } from './components/Homepage/homepage.component';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent },
+  //{ path: '', component: HomepageComponent },
   { path: 'form', loadChildren: () => import("./components/user-form/user-form.module").then(m => m.UserFormModule) },
-  { path: 'users', loadChildren: () => import("./components/users-api/users-api.module").then(m => m.UsersApiModule) },
+  { path: 'users', loadChildren: () => import("./components/users-api/users.module").then(m => m.UsersApiModule) },
   { path: 'cars', loadChildren: () => import("./components/cars/cars.module").then(m => m.CarsModule) },
   { path: 'directives', component: DirectivesComponent },
   { path: 'about', component: AboutComponent },
