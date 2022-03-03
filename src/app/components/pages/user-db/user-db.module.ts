371e0e231c9436632335db944a5c9e80fe86d9b0
Vishal-Sachan <vishal.sachan@credextechnology.com>
2022-03-03 18:36:30 +0530

    userdb component created

---
+++ src/app/components/pages/user-db/user-db.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserDbRoutingModule } from './user-db-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UserDbRoutingModule
+  ]
+})
+export class UserDbModule { }
